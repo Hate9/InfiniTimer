bf5f8b7bb2e02ed6389ccd10a2ce2a4b05d90f97
Hate9 <als.hate.09@gmail.com>
2020-10-02 17:36:03 -0400

    Initial commit

---
+++ InfiniTimer.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace InfiniTimer
+{
+    partial class InfiniTimer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.attributesList = new System.Windows.Forms.ListBox();
+            this.optionsList = new System.Windows.Forms.ListBox();
+            this.addAttribute = new System.Windows.Forms.Button();
+            this.removeAttribute = new System.Windows.Forms.Button();
+            this.addOption = new System.Windows.Forms.Button();
+            this.removeOption = new System.Windows.Forms.Button();
+            this.timerStop = new System.Windows.Forms.Button();
+            this.timerStart = new System.Windows.Forms.Button();
+            this.timerUnit = new System.Windows.Forms.ComboBox();
+            this.timerMin = new System.Windows.Forms.NumericUpDown();
+            this.timerMax = new System.Windows.Forms.NumericUpDown();
+            this.attributeName = new System.Windows.Forms.TextBox();
+            this.optionName = new System.Windows.Forms.TextBox();
+            this.attributeMax = new System.Windows.Forms.NumericUpDown();
+            this.attributeMin = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.clockTimer = new System.Windows.Forms.Timer(this.components);
+            this.randomTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.timerMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timerMax)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.attributeMax)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.attributeMin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // attributesList
+            // 
+            this.attributesList.FormattingEnabled = true;
+            this.attributesList.Location = new System.Drawing.Point(141, 103);
+            this.attributesList.Name = "attributesList";
+            this.attributesList.Size = new System.Drawing.Size(120, 121);
+            this.attributesList.TabIndex = 8;
+            // 
+            // optionsList
+            // 
+            this.optionsList.FormattingEnabled = true;
+            this.optionsList.Location = new System.Drawing.Point(267, 51);
+            this.optionsList.Name = "optionsList";
+            this.optionsList.Size = new System.Drawing.Size(120, 173);
+            this.optionsList.TabIndex = 12;
+            // 
+            // addAttribute
+            // 
+            this.addAttribute.Location = new System.Drawing.Point(204, 230);
+            this.addAttribute.Name = "addAttribute";
+            this.addAttribute.Size = new System.Drawing.Size(57, 23);
+            this.addAttribute.TabIndex = 9;
+            this.addAttribute.Text = "Add";
+            this.addAttribute.UseVisualStyleBackColor = true;
+            this.addAttribute.Click += new System.EventHandler(this.AddAttribute_Click);
+            // 
+            // removeAttribute
+            // 
+            this.removeAttribute.Location = new System.Drawing.Point(141, 230);
+            this.removeAttribute.Name = "removeAttribute";
+            this.removeAttribute.Size = new System.Drawing.Size(57, 23);
+            this.removeAttribute.TabIndex = 10;
+            this.removeAttribute.Text = "Remove";
+            this.removeAttribute.UseVisualStyleBackColor = true;
+            this.removeAttribute.Click += new System.EventHandler(this.RemoveAttribute_Click);
+            // 
+            // addOption
+            // 
+            this.addOption.Location = new System.Drawing.Point(330, 230);
+            this.addOption.Name = "addOption";
+            this.addOption.Size = new System.Drawing.Size(57, 23);
+            this.addOption.TabIndex = 13;
+            this.addOption.Text = "Add";
+            this.addOption.UseVisualStyleBackColor = true;
+            this.addOption.Click += new System.EventHandler(this.AddOption_Click);
+            // 
+            // removeOption
+            // 
+            this.removeOption.Location = new System.Drawing.Point(267, 230);
+            this.removeOption.Name = "removeOption";
+            this.removeOption.Size = new System.Drawing.Size(57, 23);
+            this.removeOption.TabIndex = 14;
+            this.removeOption.Text = "Remove";
+            this.removeOption.UseVisualStyleBackColor = true;
+            this.removeOption.Click += new System.EventHandler(this.RemoveOption_Click);
+            // 
+            // timerStop
+            // 
+            this.timerStop.Location = new System.Drawing.Point(12, 230);
+            this.timerStop.Name = "timerStop";
+            this.timerStop.Size = new System.Drawing.Size(42, 23);
+            this.timerStop.TabIndex = 4;
+            this.timerStop.Text = "Stop";
+            this.timerStop.UseVisualStyleBackColor = true;
+            this.timerStop.Click += new System.EventHandler(this.StopTimer);
+            // 
+            // timerStart
+            // 
+            this.timerStart.Location = new System.Drawing.Point(60, 230);
+            this.timerStart.Name = "timerStart";
+            this.timerStart.Size = new System.Drawing.Size(42, 23);
+            this.timerStart.TabIndex = 3;
+            this.timerStart.Text = "Start";
+            this.timerStart.UseVisualStyleBackColor = true;
+            this.timerStart.Click += new System.EventHandler(this.StartTimer);
+            // 
+            // timerUnit
+            // 
+            this.timerUnit.FormattingEnabled = true;
+            this.timerUnit.Items.AddRange(new object[] {
+            "Milliseconds",
+            "Seconds",
+            "Minutes",
+            "Hours"});
+            this.timerUnit.Location = new System.Drawing.Point(12, 178);
+            this.timerUnit.Name = "timerUnit";
+            this.timerUnit.Size = new System.Drawing.Size(90, 21);
+            this.timerUnit.TabIndex = 2;
+            this.timerUnit.Text = "Seconds";
+            // 
+            // timerMin
+            // 
+            this.timerMin.Location = new System.Drawing.Point(12, 151);
+            this.timerMin.Name = "timerMin";
+            this.timerMin.Size = new System.Drawing.Size(42, 20);
+            this.timerMin.TabIndex = 0;
+            this.timerMin.ValueChanged += new System.EventHandler(this.TimerMin_ValueChanged);
+            // 
+            // timerMax
+            // 
+            this.timerMax.Location = new System.Drawing.Point(60, 152);
+            this.timerMax.Name = "timerMax";
+            this.timerMax.Size = new System.Drawing.Size(42, 20);
+            this.timerMax.TabIndex = 1;
+            this.timerMax.ValueChanged += new System.EventHandler(this.TimerMax_ValueChanged);
+            // 
+            // attributeName
+            // 
+            this.attributeName.Location = new System.Drawing.Point(141, 25);
+            this.attributeName.Name = "attributeName";
+            this.attributeName.Size = new System.Drawing.Size(120, 20);
+            this.attributeName.TabIndex = 7;
+            // 
+            // optionName
+            // 
+            this.optionName.Location = new System.Drawing.Point(267, 25);
+            this.optionName.Name = "optionName";
+            this.optionName.Size = new System.Drawing.Size(120, 20);
+            this.optionName.TabIndex = 11;
+            // 
+            // attributeMax
+            // 
+            this.attributeMax.Location = new System.Drawing.Point(204, 77);
+            this.attributeMax.Name = "attributeMax";
+            this.attributeMax.Size = new System.Drawing.Size(57, 20);
+            this.attributeMax.TabIndex = 6;
+            // 
+            // attributeMin
+            // 
+            this.attributeMin.Location = new System.Drawing.Point(141, 77);
+            this.attributeMin.Name = "attributeMin";
+            this.attributeMin.Size = new System.Drawing.Size(57, 20);
+            this.attributeMin.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 99;
+            this.label1.Text = "From";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 99;
+            this.label2.Text = "To";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(138, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 99;
+            this.label3.Text = "Attribute Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(138, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 13);
+            this.label4.TabIndex = 99;
+            this.label4.Text = "Min";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(201, 61);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 13);
+            this.label5.TabIndex = 99;
+            this.label5.Text = "Max";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(264, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 13);
+            this.label6.TabIndex = 99;
+            this.label6.Text = "Option Name";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(138, 46);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(81, 13);
+            this.label7.TabIndex = 99;
+            this.label7.Text = "Attribute Range";
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Miriam Libre", 16F);
+            this.label8.Location = new System.Drawing.Point(12, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(123, 123);
+            this.label8.TabIndex = 99;
+            this.label8.Text = "Random Infinitron Timer";
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.timerLabel.Location = new System.Drawing.Point(12, 202);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(90, 25);
+            this.timerLabel.TabIndex = 99;
+            this.timerLabel.Text = "0:00.00";
+            this.timerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // clockTimer
+            // 
+            this.clockTimer.Interval = 10;
+            this.clockTimer.Tick += new System.EventHandler(this.ClockTimer_Tick);
+            // 
+            // randomTimer
+            // 
+            this.randomTimer.Tick += new System.EventHandler(this.RandomTimer_Tick);
+            // 
+            // InfiniTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 265);
+            this.Controls.Add(this.timerLabel);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.attributeMin);
+            this.Controls.Add(this.attributeMax);
+            this.Controls.Add(this.optionName);
+            this.Controls.Add(this.attributeName);
+            this.Controls.Add(this.timerMax);
+            this.Controls.Add(this.timerMin);
+            this.Controls.Add(this.timerUnit);
+            this.Controls.Add(this.timerStart);
+            this.Controls.Add(this.timerStop);
+            this.Controls.Add(this.removeOption);
+            this.Controls.Add(this.addOption);
+            this.Controls.Add(this.removeAttribute);
+            this.Controls.Add(this.addAttribute);
+            this.Controls.Add(this.optionsList);
+            this.Controls.Add(this.attributesList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "InfiniTimer";
+            this.Text = "InfiniTimer";
+            this.Load += new System.EventHandler(this.Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.timerMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timerMax)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.attributeMax)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.attributeMin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox attributesList;
+        private System.Windows.Forms.ListBox optionsList;
+        private System.Windows.Forms.Button addAttribute;
+        private System.Windows.Forms.Button removeAttribute;
+        private System.Windows.Forms.Button addOption;
+        private System.Windows.Forms.Button removeOption;
+        private System.Windows.Forms.Button timerStop;
+        private System.Windows.Forms.Button timerStart;
+        private System.Windows.Forms.ComboBox timerUnit;
+        private System.Windows.Forms.NumericUpDown timerMin;
+        private System.Windows.Forms.NumericUpDown timerMax;
+        private System.Windows.Forms.TextBox attributeName;
+        private System.Windows.Forms.TextBox optionName;
+        private System.Windows.Forms.NumericUpDown attributeMax;
+        private System.Windows.Forms.NumericUpDown attributeMin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label timerLabel;
+        private System.Windows.Forms.Timer clockTimer;
+        private System.Windows.Forms.Timer randomTimer;
+    }
+}
+
---
+++ InfiniTimer.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Media;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace InfiniTimer
+{
+    public partial class InfiniTimer : Form
+    {
+        private readonly SoundPlayer alarmSound = new SoundPlayer(Properties.Resources.alarm);
+        private readonly Random rng = new Random();
+        private long _clockTicks = 0;
+        public long ClockTicks
+        {
+            get => _clockTicks;
+            set
+            {
+                _clockTicks = value;
+
+                long temp = value;
+                int minutes = (int)(temp / 6000.0);
+                temp -= minutes * 6000;
+                int seconds = (int)(temp / 100.0);
+                temp -= seconds * 100;
+                int centiseconds = (int)temp;
+
+                timerLabel.Text = minutes.ToString() + ":" + seconds.ToString("D2") + "." + centiseconds.ToString("D2");
+            }
+        }
+
+        public InfiniTimer()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            Option[] options = JsonConvert.DeserializeObject<Option[]>(Properties.Settings.Default.Options);
+            if (options != null && options.Length != 0)
+            {
+                optionsList.Items.AddRange(options);
+            }
+            timerMin.Value = Properties.Settings.Default.Min;
+            timerMax.Value = Properties.Settings.Default.Max;
+            alarmSound.Load();
+        }
+
+        private void AddAttribute_Click(object sender, EventArgs e)
+        {
+            if (attributeMax.Value >= attributeMin.Value && attributeName.Text != "")
+            {
+                Attribute temp = new Attribute(attributeName.Text, (int)attributeMin.Value, (int)attributeMax.Value);
+                attributesList.Items.Add(temp);
+                attributeName.Clear();
+                attributeMin.Value = 0;
+                attributeMax.Value = 0;
+            }
+        }
+
+        private void RemoveAttribute_Click(object sender, EventArgs e)
+        {
+            if (attributesList.SelectedItem != null)
+            {
+                attributesList.Items.Remove(attributesList.SelectedItem);
+            }
+        }
+
+        private void AddOption_Click(object sender, EventArgs e)
+        {
+            if (optionName.Text != "")
+            {
+                List<Attribute> attributes = attributesList.Items.Cast<Attribute>().ToList();
+                Option temp = new Option(optionName.Text, attributes);
+                optionsList.Items.Add(temp);
+                optionName.Clear();
+                attributesList.Items.Clear();
+
+                Properties.Settings.Default.Options = JsonConvert.SerializeObject(optionsList.Items.Cast<Option>().ToArray());
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void RemoveOption_Click(object sender, EventArgs e)
+        {
+            if (optionsList.SelectedItem != null)
+            {
+                optionsList.Items.Remove(optionsList.SelectedItem);
+
+                Properties.Settings.Default.Options = JsonConvert.SerializeObject(optionsList.Items.Cast<Option>().ToArray());
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void StartTimer(object sender = null, EventArgs e = null)
+        {
+            if (!randomTimer.Enabled && !clockTimer.Enabled && timerMax.Value >= timerMin.Value && !(timerMax.Value == 0 && timerMin.Value == 0))
+            {
+                int unitMult;
+                switch (timerUnit.Text.ToLower())
+                {
+                    case "hours":
+                        unitMult = 1000 * 60 * 60;
+                        break;
+                    case "minutes":
+                        unitMult = 1000 * 60;
+                        break;
+                    default:
+                    case "seconds":
+                        unitMult = 1000;
+                        break;
+                    case "milliseconds":
+                        unitMult = 1;
+                        break;
+                }
+
+                int interval = rng.Next((int)(timerMin.Value * unitMult), (int)(timerMax.Value * unitMult));
+                if (interval <= 0)
+                {
+                    interval = 1;
+                }
+                randomTimer.Interval = interval;
+
+                randomTimer.Start();
+                clockTimer.Start();
+            }
+        }
+
+        private void StopTimer(object sender = null, EventArgs e = null)
+        {
+            randomTimer.Stop();
+            clockTimer.Stop();
+            ClockTicks = 0;
+        }
+
+        private void ClockTimer_Tick(object sender, EventArgs e)
+        {
+            ClockTicks += ((Timer)sender).Interval / 10;//might as well make the function as generic as possible, right?
+        }
+
+        private void RandomTimer_Tick(object sender, EventArgs e)
+        {
+            StopTimer();
+
+            if (optionsList.Items.Count != 0)
+            {
+                int optionNum = rng.Next(optionsList.Items.Count);
+                Option temp = (Option)optionsList.Items[optionNum];
+
+                string attributes = temp.label;
+                foreach (Attribute attribute in temp.attributes)
+                {
+                    if (attribute.min == 0 && attribute.max == 0)
+                    {
+                        attributes += "\r\n" + attribute.label;
+                    }
+                    else
+                    {
+                        attributes += "\r\n" + $"{attribute.label}: {rng.Next(attribute.min, attribute.max)}";
+                    }
+                }
+
+                alarmSound.Play();
+                MessageBox.Show(attributes, "Timer Finished!");
+                alarmSound.Stop();
+            }
+            else
+            {
+                alarmSound.Play();
+                MessageBox.Show("Timer Finished!", "Timer Finished!");
+                alarmSound.Stop();
+            }
+
+            StartTimer();
+        }
+
+        private void TimerMin_ValueChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Min = timerMin.Value;
+            Properties.Settings.Default.Save();
+        }
+
+        private void TimerMax_ValueChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Max = timerMax.Value;
+            Properties.Settings.Default.Save();
+        }
+    }
+
+    /// <summary>
+    /// Represents an evenly weighted random option, with an arbitrary number of random numeric properties (attributes).
+    /// </summary>
+    public class Option
+    {
+        public string label;
+        public List<Attribute> attributes;
+
+        public Option(string label, List<Attribute> attributes)
+        {
+            this.label = label;
+            this.attributes = attributes;
+        }
+
+        public override string ToString()
+        {
+            return label;
+        }
+    }
+
+    /// <summary>
+    /// Represents a random numeric property.
+    /// </summary>
+    public class Attribute
+    {
+        public string label;
+        public int min;
+        public int max;
+
+        public Attribute(string label, int min, int max)
+        {
+            this.label = label;
+            this.min = min;
+            this.max = max;
+        }
+
+        public override string ToString()
+        {
+            return label;
+        }
+    }
+}
